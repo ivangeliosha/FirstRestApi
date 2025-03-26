a1881ed7527353ba6b2ed1204f58be1821c89016
ivan <gopmiy36@gmail.com>
2025-03-27 00:00:19 +0300

    Final app
    Signed-off-by: ivan <gopmiy36@gmail.com>

--- src/main/java/ru/maslennikov/firstrestapi/util/PersonNotCreatedException.java
+++ src/main/java/ru/maslennikov/firstrestapi/util/PersonNotCreatedException.java
@@ -3,5 +3,5 @@ package ru.maslennikov.firstrestapi.util;
 public class PersonNotCreatedException extends Exception {
     public PersonNotCreatedException(String message) {
         super(message);
-    }
+    }//xsdcsdfvsdfvd
 }
